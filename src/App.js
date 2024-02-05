b9b9a5628e5194afce7a63ca9ec7177ce1c3f6e8
syoungee <sunyoungah69@gmail.com>
2024-02-05 14:36:01 +0900

    feature / react-router-dom settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  return <div className="App">App.js</div>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MyCart from './pages/MyCart';
+import NewProduct from './pages/NewProduct';
+import ProductDetail from './pages/ProductDetail';
+import AllProducts from './pages/AllProducts';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <App /> },
+      {
+        path: 'products',
+        element: <AllProducts />,
+      },
+      {
+        path: 'products/new',
+        element: <NewProduct />,
+      },
+      {
+        path: 'products/:id',
+        element: <ProductDetail />,
+      },
+      {
+        path: 'carts',
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
