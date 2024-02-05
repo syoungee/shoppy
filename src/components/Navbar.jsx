cda65aef6a75d48b54769b8563c5121d60382b3a
syoungee <sunyoungah69@gmail.com>
2024-02-05 15:41:36 +0900

    feature / navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ import { BsFillPencilFill } from 'react-icons/bs';
 
 export default function Navbar() {
   return (
-    <header class="flex">
-      <Link to="/">
+    <header class="flex justify-between border-b border-gray-300 p-2 font-semibold">
+      <Link to="/" class="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav>
+      <nav class="flex items-center gap-4">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        <Link to="/products/new">
+        <Link to="/products/new" class="text-2xl">
           <BsFillPencilFill />
         </Link>
         <button>Login</button>
