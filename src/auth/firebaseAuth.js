7390254a25450b6d5174afc7cdbf2e72494cee58
syoungee <sunyoungah69@gmail.com>
2024-02-06 10:53:06 +0900

    process env settings

--- src/auth/firebaseAuth.js
+++ src/auth/firebaseAuth.js
@@ -1,24 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
+// import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyATdLPcp80COCNTqLiluTBzlHKEbvC50uQ',
-  authDomain: 'shoppy-f5bf7.firebaseapp.com',
-  databaseURL: 'https://shoppy-f5bf7-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'shoppy-f5bf7',
-  storageBucket: 'shoppy-f5bf7.appspot.com',
-  messagingSenderId: '668821711406',
-  appId: '1:668821711406:web:2440c533ca0517f4b58dbf',
-  measurementId: 'G-P22NJ6GMTG',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
