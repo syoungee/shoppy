d389f3e3611f6bb897ec61293a38ee94924a9327
syoungee <sunyoungah69@gmail.com>
2024-02-14 17:55:20 +0900

    feature / Banner component 분리

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Banner() {
+  return (
+    <div className="relative mb-8">
+      <img
+        src={`https://shop-phinf.pstatic.net/20240211_137/1707621324873JvaCA_JPEG/%C1%A6%B8%F1%C0%BB_%C0%D4%B7%C2%C7%D8%C1%D6%BC%BC%BF%E4_-001_%281%29.jpg?type=m10000_10000`}
+        alt="Banner"
+        className="w-full h-400 object-cover"
+      />
+    </div>
+  );
+}
--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { readProductData } from '../auth/firebaseAuth';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import Banner from '../components/Banner';
 
 export default function Products() {
   const navigate = useNavigate();
@@ -22,13 +23,7 @@ export default function Products() {
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-      <div className="relative mb-8">
-        <img
-          src={`https://shop-phinf.pstatic.net/20240211_137/1707621324873JvaCA_JPEG/%C1%A6%B8%F1%C0%BB_%C0%D4%B7%C2%C7%D8%C1%D6%BC%BC%BF%E4_-001_%281%29.jpg?type=m10000_10000`}
-          alt="Banner"
-          className="w-full h-400 object-cover"
-        />
-      </div>
+      <Banner />
       <div className="container mx-auto p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
           {products &&
